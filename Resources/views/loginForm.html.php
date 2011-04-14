6bb22af57b64ea2059267d7d25f58bd416920593
jsor <jan@sorgalla.com>
2011-04-14 17:24:22 +0200

    Damn you IE!

--- Resources/views/loginForm.html.php
+++ Resources/views/loginForm.html.php
@@ -1,3 +1,3 @@
-<iframe class="<?php echo $class; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" src="<?php echo $loginUrl; ?>">
+<iframe class="<?php echo $class; ?>" frameborder="0" scrolling="no" width="<?php echo $width; ?>" height="<?php echo $height; ?>" src="<?php echo $loginUrl; ?>">
     <a href="<?php echo $loginUrl; ?>">DocCheck Login</a>
 </iframe>
